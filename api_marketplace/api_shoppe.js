43e13ef8e13bf4214d1cc4e749fc0376228152d3
diki_nurdiansyah <diki_nurdiansyah@smltech.co.id>
2022-02-14 17:25:53 +0700

    add json function

--- api_marketplace/api_shoppe.js
+++ api_marketplace/api_shoppe.js
@@ -1,8 +1,12 @@
-
 const axios = require('axios')
 const crypto = require('crypto')
+
 const db = require('mariadb')
 const {conf, env} = require('../conf')
+
+function jsonS(d) {return JSON.stringify(d)}
+function jsonP(d) {return d ? JSON.parse(d) : {}}
+function jsonPs(d) {return jsonP(jsonS(d))}
 
 const pl = db.createPool(conf.db)
 async function eq(q) {
