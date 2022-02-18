475b30bfc19620ca2b4d755f7357d9cc3c204543
diki_nurdiansyah <diki_nurdiansyah@smltech.co.id>
2022-02-18 10:53:28 +0700

    add async await in api shopee update token

--- api_marketplace/api_shoppe.js
+++ api_marketplace/api_shoppe.js
@@ -150,9 +150,9 @@ async function getToken(shop_id, main_account_id,code,envStore) {
       params: param,
       data: body
 
-    }).then(function (response) {
+    }).then(async function (response) {
       console.log(response);
-      const rs = eq(
+      const rs = await eq(
         `update stores set updatedby='sys_mpapi_shopee_stores', updatedtime=CURRENT_TIMESTAMP, token='${response.data.access_token}' ,refresh='${response.data.refresh_token}' where shop_id='${shop_id}' and marketplace='${envStore.marketplace}'`
       )
       if (rs && rs.text) console.log(rs)
@@ -194,11 +194,11 @@ async function getRefreshToken(shop_id, main_account_id,refresh_token,envStore) 
       params: param,
       data: body
 
-    }).then(function (response) {
+    }).then(async function (response) {
       console.log(response.config.data);
       console.log(response.data);
       if(response.data.access_token){
-        const rs = eq(
+        const rs = await eq(
           `update stores set updatedby='sys_mpapi_shopee_stores', updatedtime=CURRENT_TIMESTAMP, token='${response.data.access_token}' ,refresh='${response.data.refresh_token}' where shop_id='${shop_id}' and marketplace='${envStore.marketplace}'`
         )
         if (rs && rs.text) console.log(rs)
@@ -533,7 +533,7 @@ function getCategory(shop_id,envStore) {
 
 function getAttribute(shop_id,language,category_id,envStore) {
 
-  //path 
+  //path
   let path = "/api/v2/product/get_attributes";
   let param = {};
   param.shop_id = shop_id;
